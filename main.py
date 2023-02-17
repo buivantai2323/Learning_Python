75507d9ff6cff7986e5c6cb1f95205446a71e119
buivantai2323 <buivantai2303@gmail.com>
2023-02-17 22:17:53 +0700

    17/12/2023

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import pyodbc
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+conn_str = (
+    r'DRIVER={SQL Server};'
+    r'SERVER=BanhMiBietBay\SQLEXPRESS;'
+    r'DATABASE=Sales_Manager;'
+    r'Trusted_Connection=yes;'
+)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    DATABASE = pyodbc.connect(conn_str)
+    print("Connected successful!")
+except:
+    print("Connection not found!")
